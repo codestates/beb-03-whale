81948e8fa4bb534b042c27172ff3be3e7f282915
lhj6102 <lhj6102@gmail.com>
2022-04-13 10:53:07 +0900

    fix: TransferWhaleNFT 함수명 변경, RoomInfo함수 추가

--- contracts/WhaleNFT.sol
+++ contracts/WhaleNFT.sol
@@ -53,18 +53,17 @@ contract TransferWhaleNFT is Ownable {
         uint256 price;
         address payable sellerAddr;
         TradeStatus tradeStatus;
-        // address payable buyerAddr;
     }
 
     mapping(uint => TradeRoom) rooms;
     uint roomLen = 0;
 
-    event RoomCreated (address indexed sellerAddress, uint256 price, uint256 roomNumber);
+    event SellPosted (address indexed sellerAddress, uint256 price, uint256 roomNumber);
 
-    event Response(bool success, bytes data);
+    event TransferConfirmed (address indexed seller, address indexed buyer, uint256 price, uint256 roomNumber);
 
     // 판매등록: price 단위는 wei = ether * 10^18
-    function createRoom (address _nftContract, uint256 _tokenId, uint256 _price) public payable returns (uint roomNum) {
+    function Sell(address _nftContract, uint256 _tokenId, uint256 _price) public returns (uint roomNum) {
         // approve 되었는지 확인
         require (ERC721(_nftContract).getApproved(_tokenId) == address(this), "TransferWhaleNFT: token is not approved");
 
@@ -81,13 +80,12 @@ contract TransferWhaleNFT is Ownable {
         roomNum = roomLen;
         roomLen = roomLen + 1;
 
+        emit SellPosted(msg.sender, _price, roomNum);
+
         // owner address로 approve (컨트랙트가 같은 페이지에 없을 때)
         // (bool success, bytes memory data) = _nftContract.call {value: 1000} (
         //     abi.encodeWithSignature("approve(address, uint256)", _ownerAddr, _tokenId)
         // );
-
-        
-       
     }
 
     // function Test (address _nftContract) public returns (address sender) {
@@ -96,7 +94,7 @@ contract TransferWhaleNFT is Ownable {
 
 
     // 문제점 예상: 중간에 판매자가 임의로 approve를 변경했을 때?
-    function Buy(address _nftContract, uint256 _roomNumber)
+    function Buy(uint256 _roomNumber)
         public
         payable
     {
@@ -110,13 +108,20 @@ contract TransferWhaleNFT is Ownable {
         // 판매자에게 송금
         rooms[_roomNumber].sellerAddr.transfer(msg.value);
         // 구매자에게 nft양도
-        ERC721(_nftContract).transferFrom(rooms[_roomNumber].sellerAddr, msg.sender, tokenId);
+        ERC721(rooms[_roomNumber].nftProduct.contractAddr).transferFrom(rooms[_roomNumber].sellerAddr, msg.sender, tokenId);
 
         // idToMarketItem[itemId].owner = payable(msg.sender);
         rooms[_roomNumber].tradeStatus = TradeStatus.STATUS_COMPLETE;
         // _itemsSold.increment();
         // 수수료?
         // payable(owner).transfer(listingPrice);
+        emit TransferConfirmed (rooms[_roomNumber].sellerAddr, msg.sender, price, _roomNumber);
     }
 
+
+    // 개선할 점: 서버에 정보를 줄 수 있는 함수 생성
+    // return(status, nftcontract, tokenId, price)
+    function RoomInfo(uint256 _roomNumber) public returns (TradeStatus, address, uint256, uint256) {
+        return(rooms[_roomNumber].tradeStatus, rooms[_roomNumber].nftProduct.contractAddr, rooms[_roomNumber].nftProduct.tokenId, rooms[_roomNumber].price);
+    }
 }
