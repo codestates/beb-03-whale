e91dc5c77a3a634a1fba29989fbb2c72dfce19dc
EunsuGoh <dmstn96@gmail.com>
2022-04-14 16:39:18 +0900

    fix : 구매페이지 렌더링 오류 수정

--- whale/src/Page/Buy.js
+++ whale/src/Page/Buy.js
@@ -1,7 +1,4 @@
-import { Box, Paper, Typography } from "@material-ui/core";
-import { styled } from "@material-ui/core/styles";
-import Button from "@mui/material/Button";
-import EthereumLogo from "../images/Ethereum_logo.png";
+import { Box } from "@material-ui/core";
 import { useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -12,22 +9,19 @@ function Buy() {
   const path = location.pathname;
   const nftId = path[path.length - 1]; // 임시로 맨 끝자리만 떼왔어요~
   const [curItem, setCurItem] = useState(null);
-  const [flag, setFlag] = useState(false);
 
   async function getOneNft(tokenId) {
     let result = await axios.get(`http://localhost:4000/nft?token_id=${nftId}`);
-    setCurItem({ ...result.data });
+    setCurItem([...result.data]);
     return result.data;
   }
   useEffect(() => {
-    // console.log(getOneNft(nftId));
     getOneNft(nftId);
-    // setCurItem(resultData);
   }, []);
 
   useEffect(() => {
     console.log(curItem);
-  }, [curItem, flag]);
+  }, [curItem]);
 
   function isNull() {
     console.log(curItem);
@@ -37,7 +31,9 @@ function Buy() {
       return false;
     }
   }
-  return <Box>{isNull ? <Box></Box> : <BuyComponent curItem={curItem} />}</Box>;
+  return (
+    <Box>{isNull() ? <Box></Box> : <BuyComponent curItem={curItem} />}</Box>
+  );
 }
 
 export default Buy;
