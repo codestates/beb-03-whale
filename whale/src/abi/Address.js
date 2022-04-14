2bee1360546847bf4e7c4f43b56f939f5b544349
lhj6102 <lhj6102@gmail.com>
2022-04-14 13:29:02 +0900

    feat: abi 폴더에 컨트랙트 주소 추가

---
+++ whale/src/abi/Address.js
@@ -0,0 +1,4 @@
+const transferWhaleNFTAddress = "0xCe82f91dbC157F2f1bDC467c1BAfe97aAfc1F85c";
+const whaleNFTAddress = "0xe23E30b939a085a2d92f50C803F574c58912162B";
+
+export default { transferWhaleNFTAddress, whaleNFTAddress };
