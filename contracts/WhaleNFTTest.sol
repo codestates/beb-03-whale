c94f58bf1141273048d62eda776e9e5a2028ee00
lhj6102 <lhj6102@gmail.com>
2022-04-15 15:50:24 +0900

    fix: TransferWhaleNFT 1.0.4 설명 및 주석 추가

--- contracts/WhaleNFTTest.sol
+++ contracts/WhaleNFTTest.sol
@@ -10,12 +10,8 @@ contract WhaleNFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    // Token name and symbol
     constructor() public ERC721("WhaleNFT", "WNFT") {}
 
-    /**
-     * mintNFT and transfer to recipient
-     */
     function mintNFT(address recipient, string memory tokenURI)
         public
         returns (uint256)
@@ -29,9 +25,6 @@ contract WhaleNFT is ERC721URIStorage, Ownable {
         return newItemId;
     }
 
-    /**
-     * minNFT and transfer to sender(myself)
-     */
     function mintNFTMyself(string memory tokenURI)
         public
         returns (uint256)
@@ -46,37 +39,29 @@ contract WhaleNFT is ERC721URIStorage, Ownable {
     }
 }
 
+// contract TestWho {
+//     function Whosent () public returns (address sender) {
+//         sender = msg.sender;
+//     }
+// }
 
-/**
- * ERC721 based transfer contract
- * provides functions to register and purchase items for sale
- */
+// version 1.0.4
 contract TransferWhaleNFT {
     address private _contractOwner;
 
-    // Owner who deployed this contract
     constructor () payable {
         _contractOwner = msg.sender;
     }
 
-    /**
-     * room statuses
-     * 0: not created
-     * 1: item posted for sale
-     * 2: transfer ended
-     * 3: error    
-     */
     enum TradeStatus {  
         STATUS_NOT_CREATED, STATUS_POST, STATUS_COMPLETE, STATUS_ERROR 
     }
 
-    // NFT's contranct address and token id
     struct NFTProduct {
         address contractAddr;
         uint256 tokenId;
     }
 
-    // Room for sell NFT
     struct TradeRoom {
         NFTProduct nftProduct;
         uint256 price;
@@ -84,25 +69,31 @@ contract TransferWhaleNFT {
         TradeStatus tradeStatus;
     }
 
-    // Mapping from room list to trade room
     mapping(uint => TradeRoom) rooms;
     uint roomLen = 0;
 
-    // Emits when room has created
     event SellPosted (address indexed sellerAddress, uint256 price, uint256 roomNumber);
 
-    // Emits when transfer has ended successfully
     event TransferSuccess (address indexed seller, address indexed buyer, uint256 price, uint256 roomNumber);
 
-    /**
-     * Posts ERC721 based NFT item sender wants to sell
-     * warn: before use 'sell' transfer, NFT needs to be approved to this contract
-     * this transaction could execute by owner of NFT or owner of this contract.
-     * check if NFT got approved. if not transaction would fail
-     * returns number of room made and emits event to see information
-     */
+    // 판매등록: price 단위는 wei = ether * 10^18
+    // onlyowner? 
     function sell(address _nftContract, uint256 _tokenId, uint256 _price) public returns (uint roomNum) {
+
+        /* 
+        불가능한듯
+        // delegate call을 이용해 approve 받기
+        (bool success,) = _nftContract.delegatecall(
+            //abi.encodeWithSelector(ERC721.approve.selector, address(this), _tokenId)
+            //encodeWithSignature 안에 스페이스를 넣으면 안된다
+            abi.encodeWithSignature("approve(address,uint256)", address(this), _tokenId)
+        );
+        require (success == true, "TransferWhaleNFT: failed approve");
+        */
+
+        // nft owner 혹은 이 컨트랙트의 owner인지 확인
         require (msg.sender == ERC721(_nftContract).ownerOf(_tokenId) || msg.sender == _contractOwner,  "TransferWhaleNFT: token owner or contract owner can sell item");
+        // approve 되었는지 확인
         require (ERC721(_nftContract).getApproved(_tokenId) == address(this), "TransferWhaleNFT: token is not approved");
 
         rooms[roomLen] = TradeRoom({
@@ -113,18 +104,25 @@ contract TransferWhaleNFT {
             price: _price,
             sellerAddr: payable(msg.sender),
             tradeStatus: TradeStatus.STATUS_POST
+            // buyerAddr: payable(_ownerAddr) // will change
         });
         roomNum = roomLen;
         roomLen = roomLen + 1;
 
         emit SellPosted(msg.sender, _price, roomNum);
+
+        // owner address로 approve (컨트랙트가 같은 페이지에 없을 때)
+        // (bool success, bytes memory data) = _nftContract.call {value: 1000} (
+        //     abi.encodeWithSignature("approve(address, uint256)", _ownerAddr, _tokenId)
+        // );
     }
 
-    /**
-     * Buy posted nft item with room number
-     * needs exact value which is price of posted NFT (can find out by function roomInfo)
-     * transfer buyer's eth to seller and nft transfers to buyer
-     */
+    // function Test (address _nftContract) public returns (address sender) {
+    //     sender = TestWho(_nftContract).Whosent();
+    // }
+
+
+    // 문제점 예상: 중간에 판매자가 임의로 approve를 변경했을 때?
     function buy(uint256 _roomNumber)
         public
         payable
@@ -136,24 +134,30 @@ contract TransferWhaleNFT {
             "TransferWhaleNFT: Please submit the asking price in order to complete the purchase"
         );
 
+        // 판매자에게 송금
         rooms[_roomNumber].sellerAddr.transfer(msg.value);
+        // 구매자에게 nft양도
         ERC721(rooms[_roomNumber].nftProduct.contractAddr).transferFrom(rooms[_roomNumber].sellerAddr, msg.sender, tokenId);
+
+        // idToMarketItem[itemId].owner = payable(msg.sender);
         rooms[_roomNumber].tradeStatus = TradeStatus.STATUS_COMPLETE;
+        // _itemsSold.increment();
+        // 수수료?
+        // payable(owner).transfer(listingPrice);
         emit TransferSuccess (rooms[_roomNumber].sellerAddr, msg.sender, price, _roomNumber);
     }
 
-    /**
-     * Get room information by room number
-     * check if room is valid and returns room status, nft contract address, nft token id and price
-     */
+    // 개선할 점: 서버에 정보를 줄 수 있는 함수 생성
+    // return(status, nftcontract, tokenId, price)
+
+    // roomnumber가 초과되면 revert 
     function roomInfo(uint256 _roomNumber) public view returns (TradeStatus, address, uint256, uint256) {
+        // roomnumber가 초과되면 revert 
         require(_roomNumber < roomLen, "TransferWhaleNFT: Invalid room number");
         return(rooms[_roomNumber].tradeStatus, rooms[_roomNumber].nftProduct.contractAddr, rooms[_roomNumber].nftProduct.tokenId, rooms[_roomNumber].price);
     }
 
-    /**
-     * Get total room count
-     */
+    // count 개수의 방이 존재함
     function roomCount() public view returns (uint count) {
         count = roomLen;
     }
